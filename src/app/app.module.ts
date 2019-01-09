8ee4c59b71a067082706d13ee1f6de988afc569c
syamkumar <syam@diginxtfoundation.org>
2019-01-09 11:51:29 +0530

    moved header and footer to components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,11 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule, ErrorHandler  } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './shared/header/header.component';
-import { FooterComponent } from './shared/footer/footer.component';
+import { HeaderComponent } from './shared/components/header/header.component';
+import { FooterComponent } from './shared/components/footer/footer.component';
+import { AirbrakeErrorHandler } from 'src/app/error_handler';
 
 @NgModule({
   declarations: [
@@ -16,7 +17,9 @@ import { FooterComponent } from './shared/footer/footer.component';
     BrowserModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [
+    {provide: ErrorHandler, useClass: AirbrakeErrorHandler}
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
